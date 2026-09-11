0082dedaab636d637b1ca2c44e6fe1dccebe06db
joydip-git <joydip.mondal@gmail.com>
2026-09-11 20:12:30 +0530

    day-2 final update

--- codes/day-2/demo-app/src/app/components/another/another.ts
+++ codes/day-2/demo-app/src/app/components/another/another.ts
@@ -1,20 +1,23 @@
-import { Component, signal } from '@angular/core';
+//import { NgForOf,NgIf } from '@angular/common';
+import { Component, signal, WritableSignal } from '@angular/core';
 
 @Component({
-  imports: [],
+  //imports: [NgForOf,NgIf],
   selector: 'app-another',
   styleUrl: './another.css',
-  templateUrl: './another.html',
+  templateUrl: './another.html'
 })
 export class Another {
   title = signal<string>('Nested Component')
   txtWidth = 400
-
+  //names: WritableSignal<string[]> = signal<string[]>([])
+  names = ['anil', 'sunil', 'joy']
+  people = signal([{ id: 1, name: 'anil' }, { id: 2, name: 'sunil' }])
   // updateTitle(value: string) {
   //   //this.title = value
   //   this.title.set(value)
   // }
-  
+
   // updateTitle(e: InputEvent) {
   //   const element = e.target as HTMLInputElement
   //   this.title = element.value
