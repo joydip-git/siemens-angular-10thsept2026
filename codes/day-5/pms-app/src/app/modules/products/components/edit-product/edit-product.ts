cc3cca85a9c6d5292fda3b806983887af5c12c86
joydip-git <joydip.mondal@gmail.com>
2026-09-18 18:30:55 +0530

    edit product updated

--- codes/day-5/pms-app/src/app/modules/products/components/edit-product/edit-product.ts
+++ codes/day-5/pms-app/src/app/modules/products/components/edit-product/edit-product.ts
@@ -1,9 +1,12 @@
 import { Component, inject, OnInit, signal, WritableSignal } from '@angular/core';
 import { Product } from '../../models/product';
 import { ProductStorageService } from '../../services/product-storage-service';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { TOKEN } from '../../../../config/constants';
+import { Router } from '@angular/router';
 
 @Component({
-  imports: [],
+  imports: [ReactiveFormsModule],
   selector: 'app-edit-product',
   styleUrl: './edit-product.css',
   templateUrl: './edit-product.html',
@@ -11,12 +14,49 @@ import { ProductStorageService } from '../../services/product-storage-service';
 export class EditProduct implements OnInit {
   product = signal<Product | undefined>(undefined);
   private storageService = inject(ProductStorageService)
+  private ps = inject(TOKEN)
+  private builder = inject(FormBuilder)
+  private router = inject(Router)
+  editForm?: FormGroup;
 
   ngOnInit(): void {
     const store = this.storageService.store
     const p = store()
     if (p) {
       this.product.set(store())
+      this.editForm = this.builder.group({
+        productId: [p.productId],
+        productName: [p.productName],
+        productCode: [p.productCode],
+        description: [p.description],
+        price: [p.price],
+        releaseDate: [p.releaseDate],
+        starRating: [p.starRating],
+        imageUrl: [p.imageUrl]
+      })
+    }
+  }
+  submit() {
+    if (window.confirm('update?')) {
+      const p = this.editForm?.value as Product
+      if (p) {
+        this.ps.update(p.productId, p)
+          .subscribe({
+            next: (response) => {
+              if (response.data !== null) {
+                window.alert(response.message)
+              } else {
+                window.alert('update failed: ' + response.message)
+              }
+            },
+            error: (err) => {
+              window.alert('update failed: ' + err.message)
+            },
+            complete: () => {
+              this.router.navigate(['/products'])
+            }
+          })
+      }
     }
   }
 }
